59ea33946a6db7535210a8101566004f5c678995
BerndSeggewiss <81984865+BerndSeggewiss@users.noreply.github.com>
2024-09-08 21:03:11 +0200

    Update StrCh10Ex6 .h

--- Chapter 10/StrCh10Ex6 .h
+++ Chapter 10/StrCh10Ex6 .h
@@ -2,11 +2,11 @@
 
 class Roman_int
 {
-public:
-    Roman_int(const string& roman) : romanVal(roman) {}
-    int as_int() const;
-    friend ostream& operator << (std::ostream& os, const Roman_int& r);
+    public:
+        Roman_int(const string& roman) : romanVal(roman) {}
+        int as_int() const;
+        friend ostream& operator << (ostream& os, const Roman_int& r);
 
-private:
-    string romanVal;
+    private:
+        string romanVal;
 };
